29b1a68098646a356698d2ff4190969a719cc6d0
Mugisha-Beline <belinemugisha21@gmail.com>
2022-08-03 10:52:23 +0200

    changed the name

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,3 @@
 -- insert a new row in table
 -- inserts a new row in the table first table in my MySQL server
-INSERT into first_table (id,name) VALUEs(89,"Best School");
+INSERT into first_table (id,name) VALUEs(89,"Holberton School");
